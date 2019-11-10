2047b34268cb8caecd537764b960686c70219aed
Victor Ziv <vziv@infinidat.com>
2019-11-10 17:48:10 +0200

    Initial commit

---
+++ setvenv.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+APP=${1:-postmig}
+ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VENVDIR="$ROOTDIR/venv${APP}"
+# _________________________
+
+install_modules() {
+    cd ${ROOTDIR}
+    source ${VENVDIR}/bin/activate
+    cp "${ROOTDIR}/scripts/pip.conf" "${VENVDIR}/"
+    pip install -r ${ROOTDIR}/requirements.txt
+    deactivate
+}
+# _________________________
+
+set_environment() {
+    echo "export PYTHONPATH=${ROOTDIR}:${ROOTDIR}/${APP}" >> ${VENVDIR}/bin/activate
+}
+# _________________________
+
+main() {
+    rm -rf $VENVDIR
+    virtualenv --python $(which python3.6) --no-site-packages --clear --verbose $VENVDIR
+    set_environment
+    install_modules
+}
+# _________________________
+
+main "$@"
